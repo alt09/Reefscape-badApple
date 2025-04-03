cae24dea74576abf1911ea32099e03cb5fdd1257
Andres Lara <Andres.Lara@sisepuedefoundation.org>
2025-04-02 18:40:16 -0700

    lol

--- src/main/java/frc/robot/Subsystems/BrainRoot/Music.java
+++ src/main/java/frc/robot/Subsystems/BrainRoot/Music.java
@@ -1,54 +1,51 @@
 package frc.robot.Subsystems.BrainRoot;
 
-import java.io.File;
-
 import com.ctre.phoenix6.Orchestra;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Filesystem;
 import frc.robot.Constants.RobotStateConstants;
+import java.io.File;
 
 public class Music {
-    private final Orchestra erfgethrng = new Orchestra();
-    private final TalonFX e = new TalonFX(1, "music");
-    private final TalonFXConfiguration musicConfig = new TalonFXConfiguration();
-    
-    public Music() {
-        erfgethrng.addInstrument(e);
-        musicConfig.CurrentLimits
-            .withSupplyCurrentLimit(60)
-            .withSupplyCurrentLimitEnable(true)
-            .withStatorCurrentLimit(60)
-            .withStatorCurrentLimitEnable(true);
+  private final Orchestra erfgethrng = new Orchestra();
+  private final TalonFX e = new TalonFX(1, "music");
+  private final TalonFXConfiguration musicConfig = new TalonFXConfiguration();
 
-        e.optimizeBusUtilization();
-        e.setExpiration(100000000.00);
-        e.getConfigurator().apply(musicConfig);
+  public Music() {
+    erfgethrng.addInstrument(e);
+    musicConfig
+        .CurrentLimits
+        .withSupplyCurrentLimit(60)
+        .withSupplyCurrentLimitEnable(true)
+        .withStatorCurrentLimit(60)
+        .withStatorCurrentLimitEnable(true);
 
-        var status = 
-            erfgethrng.loadMusic(
-                Filesystem.getDeployDirectory()
-                    .toPath()
-                    .resolve("orchestra" + File.separator + "dangerzone.chrp")
-                    .toString());
-        if (!status.isOK()) {
-            System.out.println("cant load rip");
-            System.out.println("Status: " + status.toString());
-        }
+    e.optimizeBusUtilization();
+    e.setExpiration(100000000.00);
+    e.getConfigurator().apply(musicConfig);
 
-        erfgethrng.play();
-
-    }
-    public void setVoltage(double volts) {
-        e.setVoltage(
-            MathUtil.clamp(volts, -RobotStateConstants.MAX_VOLTAGE, RobotStateConstants.MAX_VOLTAGE)
-        );
+    var status =
+        erfgethrng.loadMusic(
+            Filesystem.getDeployDirectory()
+                .toPath()
+                .resolve("orchestra" + File.separator + "Never_gonna_give_you_up.mid")
+                .toString());
+    if (!status.isOK()) {
+      System.out.println("cant load rip");
+      System.out.println("Status: " + status.toString());
     }
 
-    public void setVelocity(double v_percent) {
-        setVoltage(12 * v_percent);
-}
+    erfgethrng.play();
+  }
 
+  public void setVoltage(double volts) {
+    e.setVoltage(
+        MathUtil.clamp(volts, -RobotStateConstants.MAX_VOLTAGE, RobotStateConstants.MAX_VOLTAGE));
+  }
+
+  public void setVelocity(double v_percent) {
+    setVoltage(12 * v_percent);
+  }
 }
